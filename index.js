956e4ee0d059c2c1d0a88f769c758b54fa0f6738
Nevin <n.boyd1@yahoo.com>
2025-09-08 22:05:36 -0700

    This was fun, I tried to apply the object deconstructing

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+function createLoginTracker({ username, password }) {
+  let attemptCount = 0;
+  const maxAttempts = 3;
 
+  return (inputUser, inputPass) => {
+    attemptCount++;
+
+    if (attemptCount > maxAttempts) {
+      return "Account locked due to, too many failed login attempts.";
+    }
+
+    if (inputUser === username && inputPass === password) {
+      return 'Successful login! Welcome, ${username}!';
+    } else {
+      return 'Login failed. Attempt ${attemptCount} of ${maxAttempts}.';
+    }
+  };
+}
+
+const login = createLoginTracker({ username: "user1", password: "pass1" });
+
+console.log(login("user1", "wrongpass")); //fail 1
+console.log(login("user1", "wrongpass")); //fail 2
+console.log(login("user1", "wrongpass")); //fail 3
+console.log(login("user1", "pass1"));   //passed , still locked
 
 
 module.exports = {
